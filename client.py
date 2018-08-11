942708fc99836f54d6cb611c9be7f0bb23ecce06
Anıl Aras <anilaras1@gmail.com>
2018-08-11 15:30:57 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,39 +1,63 @@
 import socket
 import pickle
-import pygame
 import time
+import sys
+import pygame
+
+argc = len(sys.argv)
+if argc < 2:
+    print("Eksik arguman girisi! \n Kullanim: \n python tcp_client.py ip_adresi \n")
+    exit()
+
+
+#####################################################################
+host = sys.argv[1]
+port = 666                   # Sunucuda kullanılan port ile aynı
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+########################### pygame joystick init ##########################################
 
 pygame.init()
 j = pygame.joystick.Joystick(0)
 j.init()
 print('Initialized Joystick : %s' % j.get_name())
 
-host = "localhost"
-port = 12347                   # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+#####################################################################
 
 
 def get():
     out = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    it = 0  # iterator
+    it = 0  
     pygame.event.pump()
-    #Read input jo
+    #stick okuma
     for i in range(0, j.get_numaxes()):
-        out[it] = int(round(j.get_axis(i) * 100))
+        out[it] = int(round(j.get_axis(i) * 400))
         it += 1
-    #Read input from buttons
+    #Butından okuma
     for i in range(0, j.get_numbuttons()):
         out[it] = j.get_button(i)
         it += 1
     return out
 
+try:
+    while True:
+        try:
+            print('Sending', get())
+            s.sendall(pickle.dumps(get()))
+        except s.error:
+            connected = False
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Bağlantı kaybedildi... yeniden bağlanılıyor \nconnection lost... reconnecting ")
+            while not connected:
+                try:
+                    s.connect((host, port))
+                    connected = True 
+                    print("re-connection successful") 
+                except socket.error:
+                    sleep(2)
+                                                          
+        time.sleep(0.1)
 
-
-while True:
-    print('Sending',get())
-    s.sendall(pickle.dumps(get()))
-    data = s.recv(1024)
-    print('Received', repr(pickle.loads(data)))
-    time.sleep(0.1)
-
+except KeyboardInterrupt:
+    print("Program kapatılıyor\nShutdown")
+    s.close()
